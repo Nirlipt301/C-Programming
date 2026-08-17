b4de824e262a6ecb0e8354892004e00cae10e8ff
Nirlipt301 <nirliptvats@outlook.com>
2026-08-18 00:35:14 +0530

    Add q2 C program

--- q2.c
+++ q2.c
@@ -4,5 +4,5 @@ main() {
     int a,b;
     printf("Enter two integers: ");
     scanf("%d %d",&a,&b);
-    printf("Sum is %d \n Difference is %d \n Product is %d \n Quotient is %.2f",a+b,a-b,a*b,a/b);
+    printf("Sum is %d,  Diff is %d,  Product is %d \n Quotient is %.2f",a+b,a-b,a*b,a/b);
 }
