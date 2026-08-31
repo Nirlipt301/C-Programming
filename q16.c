f5cd10fc726b5aa4e80b07e3eb34d922878e1a8d
Nirlipt301 <nirliptvats@outlook.com>
2026-08-31 22:11:30 +0530

    Add q16 C program

---
+++ q16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ main() {
+    int num1, num2, num3;
+
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 >= num2 && num1 >= num3) {
+        printf("Largest is %d\n", num1);
+    } else if (num2 >= num1 && num2 >= num3) {
+        printf("Largest is %d\n", num2);
+    } else {
+        printf("Largest is %d\n", num3);
+    }
+
+}
