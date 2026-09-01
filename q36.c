91aaff5463089cc422832d1efdb26bd4e59de42a
Nirlipt301 <nirliptvats@outlook.com>
2026-09-01 22:13:00 +0530

    Add q36 C program

---
+++ q36.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+    int a, b, temp;
+
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d\n", a);
+}
