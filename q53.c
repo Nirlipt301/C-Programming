db54974b24602164e8ecb4f4d80f61b642c2c283
Nirlipt301 <nirliptvats@outlook.com>
2026-09-13 23:52:23 +0530

    Add q53.c

---
+++ q53.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main() {
+
+    for (int i = 1; i <= 5; i++) {
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for (int i = 4; i >= 1; i--) {
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+}
